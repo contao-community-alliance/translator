21d3075f4c2da63a1ee0cf1002a38a246ae92314
Philipp Lorenz <philipp.lorenz@etes.de>
2019-12-20 09:54:25 +0100

    Add author to BackportedTranslator class

--- src/BackportedTranslator.php
+++ src/BackportedTranslator.php
@@ -13,6 +13,7 @@
  * @package    contao-community-alliance/translator
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Philipp Lorenz <philipp.lorenz@etes.de>
  * @copyright  2013-2018 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/translator/blob/master/LICENSE LGPL-3.0
  * @filesource
