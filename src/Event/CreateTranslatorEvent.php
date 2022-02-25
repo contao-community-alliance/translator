446c0555d5cdf4583f33860fb7dfb57166f9455e
Ingolf Steinhardt <git@e-spin.de>
2022-02-25 19:34:50 +0100

    Fix symfony dependency

--- src/Event/CreateTranslatorEvent.php
+++ src/Event/CreateTranslatorEvent.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/translator.
  *
- * (c) 2013-2018 Contao Community Alliance.
+ * (c) 2013-2022 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2013-2018 Contao Community Alliance.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2013-2022 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/translator/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -22,7 +23,7 @@
 namespace ContaoCommunityAlliance\Translator\Event;
 
 use ContaoCommunityAlliance\Translator\TranslatorInterface;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Class CreateTranslatorEvent is emitted when the translator is created.
