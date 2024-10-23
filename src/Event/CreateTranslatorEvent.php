779f4012109c05fd23dba17930074204eb714ec8
Ingolf Steinhardt <info@e-spin.de>
2024-10-23 08:08:47 +0200

    Fix PHPCQ

--- src/Event/CreateTranslatorEvent.php
+++ src/Event/CreateTranslatorEvent.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/translator.
  *
- * (c) 2013-2022 Contao Community Alliance.
+ * (c) 2013-2024 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,7 @@
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2013-2022 Contao Community Alliance.
+ * @copyright  2013-2024 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/translator/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -38,6 +38,8 @@ class CreateTranslatorEvent extends Event
      * The event name.
      *
      * @var string
+     *
+     * @psalm-suppress MissingClassConstType
      */
     public const NAME = 'contao-community-alliance.translator.create-translator';
 
